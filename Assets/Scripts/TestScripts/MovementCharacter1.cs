c2ad50c4cc14d53337093dbb4f9136e182a3a60b
CERBO1499 <47153483+CERBO1499@users.noreply.github.com>
2020-11-06 08:47:16 -0500

    Carlos: BezierPaths creados

---
+++ Assets/Scripts/TestScripts/MovementCharacter1.cs
@@ -0,0 +1,100 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCharacter1 : MonoBehaviour
+{
+
+    Rigidbody rbPlayer;
+    float _dirx;
+    float width;
+    [Header("Movimiento con giroscopio")]
+    [SerializeField] float _moveSpeed = 20f;
+    [SerializeField] float _moveSpeedGeneral;
+    [Header("Movimiento con touch")]
+    [SerializeField] float _moveSpeedTouh;
+    [SerializeField]
+    AnimationCurve curveMovement;
+
+    [Space]
+    #region Components
+    [Header("Components")]
+    [SerializeField]
+    CinemachineVirtualCamera virtualCamera;
+    #endregion
+
+    private void Awake()
+    {
+        width = Screen.width;
+    }
+
+    private void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            _dirx = Input.GetAxisRaw("Horizontal") * _moveSpeed;
+
+            rbPlayer.velocity = (transform.right * _dirx);
+        }
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            // movementWithAcelerometer();
+
+            if (Input.touches.Length > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                // Movimiento
+                if (touch.position.x <= (width / 2) - (0.1f * width))
+                {
+                    if (transform.localPosition.x > 0)
+                        rbPlayer.velocity = -transform.right * _moveSpeed;
+                    else if(transform.localPosition.x <= 0)
+                        rbPlayer.velocity = (-transform.right * _moveSpeed) * curveMovement.Evaluate(1 - (Mathf.Abs(transform.localPosition.x) / 20));
+                    
+                }
+                else if (touch.position.x >= (width / 2) + (0.1f * width))
+                {
+                    if (transform.localPosition.x < 0)
+                        rbPlayer.velocity = transform.right * _moveSpeed;
+                    else if (transform.localPosition.x >= 0)
+                         rbPlayer.velocity = (transform.right * _moveSpeed) * curveMovement.Evaluate(1 - (Mathf.Abs(transform.localPosition.x) / 20));
+                    
+                }
+
+                //Rotacion
+                if (touch.position.x <= (width / 2) - (0.1f * width))
+                {
+                    virtualCamera.m_Lens.Dutch -= 0.1f;
+
+                    if (virtualCamera.m_Lens.Dutch < -12.5f)
+                        virtualCamera.m_Lens.Dutch = -12.5f;
+                }
+                else if (touch.position.x >= (width / 2) + (0.1f * width))
+                {
+                    virtualCamera.m_Lens.Dutch += 0.1f;
+
+                    if (virtualCamera.m_Lens.Dutch > 12.5f)
+                        virtualCamera.m_Lens.Dutch = 12.5f;
+                }
+            }
+            else
+                rbPlayer.velocity = Vector3.zero;
+        }
+    }
+
+
+
+    void movementWithAcelerometer()
+    {
+        _dirx = Input.acceleration.x * _moveSpeed;
+
+        rbPlayer.velocity = (transform.right * _dirx);
+    }
+}
---
+++ Assets/Scripts/TestScripts/Movement_TestBezier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Movement_TestBezier : MonoBehaviour
+{
+   
+    [Header("Variables movimiento")]
+    [SerializeField]
+    float movementSpeed=5;
+    [SerializeField]
+    PathCreator pathCreator;
+
+    float distanceTraveled;
+
+    private void FixedUpdate()
+    {
+        distanceTraveled += movementSpeed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTraveled);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTraveled);
+    }
+}
