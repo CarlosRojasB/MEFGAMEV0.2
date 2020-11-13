f3d6aa5f1595717b9d813faf04a54d09f65c8c9d
CERBO1499 <47153483+CERBO1499@users.noreply.github.com>
2020-11-13 09:32:07 -0500

    InitialTrivia

---
+++ Assets/Scripts/Trivia/ActiveButtonForTrivia.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveButtonForTrivia : MonoBehaviour
+{
+    #region Information
+    [SerializeField] Transform btnForTrivia;
+    [SerializeField] Camera mycamera;
+    [SerializeField] GameObject Trivia;
+    #endregion
+
+    private void Start()
+    {
+        mycamera = Camera.main;
+       
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Vector3 CameraCenter = mycamera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, mycamera.nearClipPlane));
+        if (Physics.Raycast(CameraCenter, transform.forward, out hit))
+        {
+            btnForTrivia.gameObject.SetActive(true);
+        }
+        else
+        {
+            btnForTrivia.gameObject.SetActive(false);
+        }
+    }
+
+
+    public void GoToTrivia()
+    {
+        Trivia.SetActive(true);
+    }
+
+
+
+}
---
+++ Assets/Scripts/Trivia/TriviaManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriviaManager : MonoBehaviour
+{
+    #region Information
+    [Header("Movement UI information",order =0)]
+    [SerializeField]
+    RectTransform trivia;
+    [SerializeField]
+    AnimationCurve curve;
+    #endregion
+
+   public void UserResponse()
+   {
+        StartCoroutine(ActiveHistory());
+   }
+  
+   IEnumerator ActiveHistory()
+   {
+        trivia.gameObject.SetActive(true);
+
+        Vector2 initialPosition = Vector3.zero;
+        Vector2 finalPosition = new Vector3(0f, 15f, 0f);
+
+        float t = Time.time;
+        while (Time.time <= t + 1f)
+        {
+            trivia.anchoredPosition = initialPosition + ((finalPosition - initialPosition) * curve.Evaluate(Time.time - t));
+            yield return null;
+        }
+        trivia.anchoredPosition = finalPosition;
+   }
+
+
+
+}
