f071a55f70ac1080d099e0e02a752054c782f2b7
CERBO1499 <47153483+CERBO1499@users.noreply.github.com>
2020-11-18 13:19:17 -0500

    Carlos-Hummin

---
+++ Assets/Scripts/FondoAR/ApearImage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApearImage : MonoBehaviour
+{
+    [SerializeField] RectTransform ImageToAppear;
+    [SerializeField] AnimationCurve curve;
+
+    private void OnEnable()
+    {
+        StartCoroutine(ActiveImage());
+    }
+
+    public IEnumerator ActiveImage()
+    {
+        ImageToAppear.gameObject.SetActive(true);
+        Vector2 initialPosition = new Vector3(0f,-2171f,0f);
+        Vector2 finalPosition = Vector3.zero;
+        float t = Time.time;
+        while (Time.time <= t + 1f)
+        {
+            ImageToAppear.localPosition = initialPosition - ((finalPosition + initialPosition) * curve.Evaluate(Time.time - t));
+            yield return null;
+        }
+
+        ImageToAppear.anchoredPosition = finalPosition;
+        
+    }
+}
